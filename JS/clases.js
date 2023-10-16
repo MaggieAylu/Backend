785ff2db9c191587eabb22647cc671915eb9ee3b
Maggie D <magaayludiaz@gmail.com>
2023-10-16 20:02:21 -0300

    first commit

---
+++ JS/clases.js
@@ -0,0 +1,34 @@
+class ProductManager{
+    constructor(){
+        this.products = [];
+    }
+    addProducts(title, description, price, thumbnail, stock){
+       let code = 1
+       if(this.products.length>0){
+        code = this.products[this.products.length-1].code + 1
+       }
+       
+        let nuevoProducto ={
+            code,
+            title,
+            description,
+            price,
+            thumbnail,
+            stock
+        }
+
+        this.products.push(nuevoProducto)
+    }
+    getProducts(){
+        return this.products;
+    }
+    getProductById(id){
+        let indice = this.products.findIndex(producto=>producto.id===id)
+        return this.products
+    }
+}
+
+let pm = new ProductManager()
+pm.addProducts('remera', 'remera de manga corta color azul', 2000, 'imagenes\remera.webp', 30 )
+pm.addProducts('short', 'short de jean', 5000, 'imagenes\short.jpeg', 15)
+console.log(pm.getProducts())
