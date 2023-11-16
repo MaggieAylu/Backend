19b4227819fcbb66bde2f2e5b64a217e617619d4
Maggie D <magaayludiaz@gmail.com>
2023-11-16 12:20:57 -0300

    Cambios

--- src/routes/ViewsRoutes.js
+++ src/routes/ViewsRoutes.js
@@ -3,17 +3,17 @@ import { productManager } from "../index.js"
 export const router=Router()
 
 
-router.get('/', (req,res)=>{
+router.get('/', async (req,res)=>{
 
-    let productos = productManager.getProducts()
+    let productos = await productManager.getProducts()
     res.status(200).render('home', {
         productos: productos
     })
 })
 
-router.get('/realtimeproducts',(req,res)=>{
+router.get('/realtimeproducts', async (req,res)=>{
 
-    let productos=productManager.getProducts()
+    let productos= await productManager.getProducts()
 
     res.status(200).render('realTimeProducts',{
         productos: productos 
